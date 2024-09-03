815e7c4b44ea9ff91d9021d264f7c43e6a746f26
傅业丙 <10208375+fuyebing@user.noreply.gitee.com>
2024-09-03 12:01:01 +0800

    优化通信

--- Code/net/gobang_network.h
+++ Code/net/gobang_network.h
@@ -3,6 +3,8 @@
 
 #include <iostream>
 #include <QThread>
+#include <QTcpServer>
+#include <QTcpSocket>
 
 #define log(fmt,...)    qDebug("\e[42m[%s] %s:%d\e[0m--"#fmt"", __FILE__, __FUNCTION__, __LINE__, ##__VA_ARGS__)
 #define err(fmt,...)    qDebug("\e[41m[%s] %s:%d\e[0m--"#fmt"", __FILE__, __FUNCTION__, __LINE__, ##__VA_ARGS__)
@@ -12,8 +14,26 @@ class Gobang_Network: public QThread
 public:
     Gobang_Network();
 
+private slots:
+    void onReadMessage();   //处理服务端反馈的数据
+    void onDisplayError(QAbstractSocket::SocketError e);    //打印错误信息
+    void connected();
+private:
+    uint8_t linkSever();
+public:
+    uint8_t sendBuff(uint8_t *buff, int len);
+    uint8_t sendString(QString str);
+private:
+    QString host = "192.168.88.1";
+    quint16 post = 3304;
+    QTcpSocket* tcpSocket;  //tcp客户端
+
 protected:
     void run();
 };
+
+
+
+
 
 #endif // GOBANG_NETWORK_H
